f26761b1bd0d7b24a383ac4d0c8fd5ea019e2cc7
Nancy De Smet <ciek.eu@gmail.com>
2016-08-18 11:35:20 +0200

    age predictor 2nd version

--- src/main/java/be/vdab/nancy/AgePredictor.java
+++ src/main/java/be/vdab/nancy/AgePredictor.java
@@ -17,15 +17,35 @@ public class AgePredictor extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter out = resp.getWriter();
-        out.println("<!DOCTYPE html");
-        out.println("<html lang= 'en'>");
-        out.println("<head>");
-        out.println("<meta charset='UTF-8'/>");
-        out.println("</head>");
-        out.println("<body>");
-        out.println("<h1> Hello, " + req.getParameter("name") + " <br>Your age is: "+req.getParameter("age")+ "</h1>");
-        out.println("</body>");
-        out.println("<html>");
+
+
+       // resp.setHeader("kevin", "Hallo");
+        resp.setHeader("Content-Type", "text/plain");
+
+        //String agent = req.getHeader("User-Agent");
+        resp.setHeader("Content-Type", "image/jpeg");
+
+        System.out.println(req.getHeader("Host"));
+
+        String ageString = req.getParameter("age");
+        try{
+            int ageInt = Integer.parseInt(ageString);
+            out.println("<!DOCTYPE html>");
+            out.println("<html lang= 'en'>");
+            out.println("<head>");
+            out.println("<meta charset='UTF-8'/>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1> Hello, " + req.getParameter("name") + " <br>Your age next year will be: "+(Integer.parseInt(req.getParameter("age"))+1)+ "</h1>");
+            out.println("</body>");
+            out.println("<html>");
+        }
+        catch (NumberFormatException e){
+            out.println("ongeldige invoer");
+
+        }
+
+
 
 
     }
